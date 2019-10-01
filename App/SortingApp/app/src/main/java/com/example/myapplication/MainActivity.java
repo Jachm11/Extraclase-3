fc424f427849001e509a125c91626a8d7007fa68
Jose Alejandro Chavarria <joalchama@gmail.com>
2019-09-30 19:01:18 -0600

    pantalla principal done

--- App/SortingApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ App/SortingApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -22,6 +23,10 @@ import java.util.LinkedList;
 public class MainActivity extends AppCompatActivity {
 
     LinkedList<Integer> inputs = new LinkedList<>();
+    int sortType = 0; // 0 null, 1 bubble, 2 selection, 3 insertion
+    int swaps = 0;
+    int comparaciones = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +36,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        //Boton de sort
-        Button sort = findViewById(R.id.sort);
-        sort.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("hola");
-            }
-        });
-
         //Text input
         final EditText entrie = findViewById(R.id.entries);
 
@@ -48,16 +44,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Boton de add
-        Button add = findViewById(R.id.add);
+        final Button add = findViewById(R.id.add);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println(entrie.getText());
-                Integer integer = Integer.parseInt((entrie.getText()).toString());
-                inputs.add(integer);
-                entrie.setText(null);
-                lista.setText(inputs.toString());
-                
+                if (!(entrie.getText().toString().equals(""))) {
+                    Integer integer = Integer.parseInt((entrie.getText()).toString());
+                    inputs.add(integer);
+                    entrie.setText(null);
+                    lista.setText(inputs.toString());
+
+                }
             }
         });
 
@@ -68,6 +65,70 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 inputs = new LinkedList<>();
                 lista.setText(inputs.toString());
+            }
+        });
+
+        //Radio group
+        RadioButton bubble = findViewById(R.id.bubble);
+        bubble.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortType = 1;
+                System.out.println(sortType);
+            }
+        });
+
+        RadioButton selection = findViewById(R.id.selection);
+        selection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortType = 2;
+                System.out.println(sortType);
+            }
+        });
+
+        RadioButton insertion = findViewById(R.id.insertion);
+        insertion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortType = 3;
+                System.out.println(sortType);
+            }
+        });
+
+
+        //Boton de sort
+        final Button sort = findViewById(R.id.sort);
+        sort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Integer[] array = inputs.toArray(new Integer[inputs.size()]);
+                System.out.println(array);
+
+                switch (sortType){
+                    case 1:
+                        //Llamada al metodo void de Bubble(array)
+
+                    case 2:
+                        //Llamada al metodo void de Selection(array)
+
+                    case 3:
+                        //Llamada al metodo void de Insertion(array)
+
+                // Para estos metodos opino q lo mejor esq modifiquen variables en su clase
+                // Para a hacer algo asi: simpre y cuando los metods de arriba esten en una clase.
+                //Sino es lo mismo pero para cada metodo
+
+                /*
+                  comparaciones = claseDelMetodo.getComps();
+                  swaps = claseDelMetodo.getSwaps();
+                 */
+                }
+
+                // Ejecuta el metodo que muestra en la pantalla lo que queremos
+                //Puede ser algo como: mostrar(comparaciones,swaps,array de arrays)
+
             }
         });
 
